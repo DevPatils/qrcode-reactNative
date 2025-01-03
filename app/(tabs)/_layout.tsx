bfbe84b545a5e6b1e3aab1c7b2676b04669e9ee3
Dishant1804 <dishantmiyani1804@gmail.com>
2025-01-03 20:18:43 +0530

    ui landing and scan page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,87 +6,91 @@ import AntDesign from '@expo/vector-icons/AntDesign';
 import IonIcons from '@expo/vector-icons/Ionicons';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
-const TabIcon = ({ icon, color, name, focused }:any) => {
-    return (
-        <View className="items-center justify-center gap-2">
-            <Image
-                source={icon}
-                resizeMode="contain"
-                style={{ tintColor: color }}
-                className="w-6 h-6 m-auto mt-8"
-            />
-            <Text
-                className={`w-20 text-center ${focused ? 'font-bold' : 'font-medium'} text-xs ${color}`}
-            >
-                {name}
-            </Text>
-        </View>
-    );
+const TabIcon = ({ icon, color, name, focused }: any) => {
+  return (
+    <View className="items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        style={{ tintColor: color }}
+        className="w-6 h-6 m-auto mt-8"
+      />
+      <Text
+        className={`w-20 text-center ${focused ? 'font-bold' : 'font-medium'} text-xs ${
+          focused ? 'text-[#799937]' : 'text-black'
+        }`}
+      >
+        {name}
+      </Text>
+    </View>
+  );
 };
 
 const TabLayout = () => {
-    return (
-        <>
-            <Tabs
-                screenOptions={{
-                    tabBarShowLabel: true,
-                    tabBarActiveTintColor: "#FFA001",
-                    tabBarInactiveTintColor: "#CDCDCD",
-                    tabBarStyle: {
-                        backgroundColor: "#161622",
-                        borderTopWidth: 4,
-                        borderTopColor: "#000000",
-                        height: 90,
-                        shadowColor: "#000000",
-                        shadowOffset: { width: 0, height: 4 },
-                        shadowOpacity: 0.2,
-                        shadowRadius: 4,
-                    },
-                }}
-            >
-                <Tabs.Screen
-                    name="market"
-                    options={{
-                        title: "Market",
-                        headerShown: false,
-                        tabBarIcon: ({ color }) => (
-                            <AntDesign name="shoppingcart" size={24} color={color} className="bg-[#FFA001] p-1 rounded-sm" />
-                        ),
-                    }}
-                />
-                <Tabs.Screen
-                    name="scan"
-                    options={{
-                        title: "Scan",
-                        headerShown: false,
-                        tabBarIcon: ({ color }) => (
-                            <IonIcons name="scan" size={24} color={color} className="bg-[#E0F7FA] p-1 rounded-sm" />
-                        ),
-                    }}
-                />
-                <Tabs.Screen
-                    name="tutorials"
-                    options={{
-                        title: "Tutorial",
-                        headerShown: false,
-                        tabBarIcon: ({ color }) => (
-                            <MaterialIcons name="travel-explore" size={24} color={color} className="bg-[#FFCDD2] p-1 rounded-sm" />
-                        ),
-                    }}
-                />
-                <Tabs.Screen
-                    name="profile"
-                    options={{
-                        title: "Profile",
-                        headerShown: false,
-                        tabBarIcon: ({ color }) => (
-                            <AntDesign name="user" size={24} color={color} className="bg-[#C8E6C9] p-1 rounded-sm" />
-                        ),
-                    }}
-                />
-            </Tabs>
-        </>
-    );
+  return (
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: true,
+          tabBarActiveTintColor: "#06281b", // green-pea-200 color
+          tabBarInactiveTintColor: "#000000",
+          tabBarPosition: 'bottom',
+          tabBarStyle: {
+            paddingTop: 5,
+            borderRadius: 15,
+            backgroundColor: "#FFFFFF",
+            borderTopColor: "#000000",
+            height: 60,
+            shadowColor: "#000000",
+            shadowOffset: { width: 0, height: 4 },
+            shadowOpacity: 0.2,
+            shadowRadius: 3,
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="market"
+          options={{
+            title: "Market",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <AntDesign name="shoppingcart" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="scan"
+          options={{
+            title: "Scan",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <IonIcons name="scan" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="tutorials"
+          options={{
+            title: "Tutorial",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <MaterialIcons name="travel-explore" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <AntDesign name="user" size={24} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
+  );
 };
 
 export default TabLayout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,47 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
-      
+      fontFamily: {
+        'gilroy-regular': ['Gilroy-Regular', 'sans-serif'],
+        'gilroy-regular-italic': ['Gilroy-RegularItalic', 'sans-serif'],
+        'gilroy-medium': ['Gilroy-Medium', 'sans-serif'],
+        'gilroy-light': ['Gilroy-Light', 'sans-serif'],
+        'gilroy-medium-italic': ['Gilroy-MediumItalic', 'sans-serif'],
+        'gilroy-bold': ['Gilroy-Bold', 'sans-serif']
+      },
+      colors: {
+        'kaitoke-green': {
+          '50': '#edfff6',
+          '100': '#d4ffec',
+          '200': '#adffda',
+          '300': '#6dffbe',
+          '400': '#27ff9b',
+          '500': '#00ec7c',
+          '600': '#00c563',
+          '700': '#009951',
+          '800': '#037843',
+          '900': '#044e2e',
+          '950': '#00381e',
+        },
+        'green-pea': {
+          '50': '#eefbf3',
+          '100': '#d5f6e0',
+          '200': '#aeecc5',
+          '300': '#79dca5',
+          '400': '#43c481',
+          '500': '#20a967',
+          '600': '#138852',
+          '700': '#0f6d43',
+          '800': '#10603d',
+          '900': '#0d472f',
+          '950': '#06281b',
+        },
+      }
     },
   },
   plugins: [],
